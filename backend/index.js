6ef71abb3fef84c650e159e212b556130e591220
SherazHashmi1111 <sherazhashmi111@gmail.com>
2025-09-11 14:28:08 +0500

    npm installed Basic server has been created

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
-
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173/";
 const app = express();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
