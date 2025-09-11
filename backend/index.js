bb9b461c78337d3d1ddf268e265eeba56e402620
SherazHashmi1111 <sherazhashmi111@gmail.com>
2025-09-11 16:44:15 +0500

    Database has been connected successfully

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 import cors from "cors";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -15,5 +16,10 @@ app.use(
     credentials: true,
   })
 );
+
+mongoose
+  .connect(process.env.MONGODB_URI, {dbName: "mern-blog"})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
